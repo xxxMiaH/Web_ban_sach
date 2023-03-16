fafaa9294455b3403cf7583c8f873576beaa4c1e
ttD4T <jsmsj04@gmail.com>
2023-03-16 21:36:13 +0700

    Fix CORS

--- src/server.ts
+++ src/server.ts
@@ -51,8 +51,7 @@ const bootServer = () => {
          credentials: true,
          origin: [
             'http://localhost:5173',
-            'http://localhost:3000',
-            'https://ban-sach-truc-tuyen.vercel.app',
+            // 'https://ban-sach-truc-tuyen.vercel.app',
          ],
          allowedHeaders: [
             'Authorization',
