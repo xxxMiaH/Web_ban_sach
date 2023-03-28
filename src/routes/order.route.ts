490301821e213c4bfdc4eb21e992f140eec70f3b
Chu Quang Hải <76799685+quanghaik2@users.noreply.github.com>
2023-03-28 10:48:21 +0700

    fix bug Order

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -3,17 +3,12 @@ const router = express.Router();
 
 import OrderController from '../controllers/order.controller';
 
-router.route('/statistics').get(OrderController.getProductOrder);
+router.get('/statistics', OrderController.getProductOrder);
 router
    .route('/:id')
    .get(OrderController.getAOrder)
-<<<<<<< HEAD
    .patch(OrderController.updateAOrder)
    .delete(OrderController.deleteAOrder)
-=======
-   .put(OrderController.updateAOrder)
-   .delete(OrderController.deleteAOrder);
->>>>>>> 9fadc6c19d085fdfef61f6bc3a1515107c89fec6
 router
    .route('/')
    .get(OrderController.getAllOrder)
